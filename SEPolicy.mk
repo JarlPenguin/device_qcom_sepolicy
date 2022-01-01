78e9bba450c36767059cfe0adcf37ff69f448f51
Jarl-Penguin <jarlpenguin@outlook.com>
2022-01-01 11:11:03 +0300

    Revert "sepolicy: Include Lineage-specific QCOM sepolicy"
    This reverts commit a6a27dc4af7601564bab00d7a29b03d9f03a6fc4.

--- SEPolicy.mk
+++ SEPolicy.mk
@@ -47,5 +47,3 @@ ifneq (,$(filter sdm845 sdm710 sdm660 msm8937 msm8953 msm8996 msm8998, $(TARGET_
     BOARD_SEPOLICY_DIRS += $(SEPOLICY_PATH)/legacy/vendor/test
     endif
 endif
-
--include device/lineage/sepolicy/qcom/sepolicy.mk
